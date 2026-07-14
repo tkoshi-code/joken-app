0cf3bfca7de708450d388ad31aea51c11a94d977
tkoshi <t.koshi54@gmail.com>
2026-07-14 12:59:38 +0900

    診断ログとアクセス解析を追加 (v1.30)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'joken-v32';
+const CACHE = 'joken-v34';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
